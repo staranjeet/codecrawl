9a20f02f5c00337ad87f6e240ea9daca1b879fa2
Taranjeet <reachtotj@gmail.com>
2015-01-24 20:56:54 +0530

    initial settings done

--- codecrawl/settings.py
+++ codecrawl/settings.py
@@ -31,6 +31,9 @@ ADMINS = (
      ('Taranjeet','reachtotj@gmail.com'),
 )
 
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 # Application definition
 
 INSTALLED_APPS = (
@@ -40,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'codecal',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -62,17 +66,28 @@ WSGI_APPLICATION = 'codecrawl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codecrawldb',
+        'USER':'tj',
+        'PASSWORD':'gameover',
     }
 }
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATICFILES_DIRS = (
+            os.path.join( PROJECT_ROOT, 'static'),
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -83,5 +98,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+TEMPLATE_DIRS = [(os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'codecaltemplates'))]
 
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
